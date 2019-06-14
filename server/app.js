51f2be1aa461de60e7ad0132f597211fdd83b783
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-14 15:45:53 -0700

    proxy is fully setup and respinding

--- server/app.js
+++ server/app.js
@@ -1,26 +1,29 @@
+require('newrelic');
+
 const express = require('express');
-const proxy = require('http-proxy-middleware');
+const request = require ('request');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const { routes } = require('./config.json');
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(cors());
 
-for (route of routes) {
-  app.use(route.route,
-    proxy({
-      target: route.address,
-      pathRewrite: (path, req) => {
-        return path.split('/').slice(2).join('/');
-      }
-    })
-  );
-}
+
+app.get('/listing/:id', (req, res) => {
+  let {id} = req.params;
+  request("http://127.0.0.1:3002/listing/" + id  ,(error, response, body) => {
+    if (error) {
+      res.status(404).send(error);
+    } else {
+      res.status(200).send(body);
+    }
+  });
+});
 
 app.listen(3030, () => {
   console.log('Proxy listening on port 3030');
