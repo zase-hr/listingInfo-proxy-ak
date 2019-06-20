c9ba259237699916172d8b6a928e03714785a584
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-06-19 22:28:06 -0700

    fixing the right address for local LB

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(cors());
 
-const LB_ADDRESS = '/api';  // http://172.31.46.42:3002
+const LB_ADDRESS = 'http://localhost/api';  // http://172.31.46.42:3002
 
 let bundleContent;
 request(LB_ADDRESS+"/bundle.js", (err, response, body) => {
